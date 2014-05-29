1c2fc0b3a62a84cb3fe85363f50f126ff968a59b
Matthew Webber <matthew.webber@diamond.ac.uk>
2014-05-29 12:06:48 +0100

    Improved handling of tagging and branching repositories Remove premable code (no longer used); add include and exclude patterns

--- diamond.releng.jenkins/job.scripts/make_branch_tag_generate_script.py
+++ diamond.releng.jenkins/job.scripts/make_branch_tag_generate_script.py
@@ -12,7 +12,7 @@ import time
 
 def generate_make_branch_tag_script():
 
-    REPOSITORIES_TO_IGNORE = ()
+    REPOSITORIES_TO_ALWAYS_IGNORE = ('diffcalc.git',)
 
     # set up environment
     if not len(sys.argv) == 2:
@@ -56,63 +56,62 @@ git branch -a'''
     BASH_GIT_TAG = \
 '''# make tag at specified commit
 git checkout master
-git tag -a -m "%(tag_commitmsg)s" %(tag_name_preamble)s%(tag_name)s %(HEAD)s
+git tag -a -m "%(tag_commitmsg)s" %(tag_name)s %(HEAD)s
 git push --verbose --tags'''
 
     BASH_GIT_BRANCH = \
 '''# make branch at specified commit
 git checkout master
-git checkout -b %(branch_name_preamble)s%(branch_name)s %(HEAD)s
+git checkout -b %(branch_name)s %(HEAD)s
 git status
-git push --verbose -u origin %(branch_name_preamble)s%(branch_name)s
+git push --verbose -u origin %(branch_name)s
 git branch -a'''
 
     tag_name = os.environ.get('tag_name')
     tag_commitmsg = os.environ.get('tag_commitmsg')
     branch_name = os.environ.get('branch_name')
 
-    tag_name_preamble = os.environ.get('tag_name_preamble','')
-    tag_name_preamble_repos = os.environ.get('tag_name_preamble_repos')
-    branch_name_preamble = os.environ.get('branch_name_preamble','')
-    branch_name_preamble_repos = os.environ.get('branch_name_preamble_repos')
+    repository_names_to_include_pattern = os.environ.get('repository_names_to_include_pattern','')
+    repository_names_to_exclude_pattern = os.environ.get('repository_names_to_exclude_pattern','')
 
     github_anonymous_prefix = 'git://github.com/'
     github_authenticated_prefix = 'git@github.com:'
 
     # generate the bash script to branch the Git repositories
     print('### File generated on %s\n###' % (time.strftime("%a, %Y/%m/%d %H:%M:%S"),))
+    for name in (
+    'repository_names_to_include_pattern',
+    'repository_names_to_exclude_pattern',
+    ):
+        print("### %-35s = %s" % (name, locals()[name]))
     if tag_name and tag_commitmsg:
-        print('### make tags in Git repositories')
+        print('###\n### make tags in Git repositories')
         for name in (
         'tag_name',
         'tag_commitmsg',
-        'tag_name_preamble',
-        'tag_name_preamble_repos',
         ):
-            print("### %-26s = %s" % (name, locals()[name]))
+            print("### %-35s = %s" % (name, locals()[name]))
     if branch_name:
-        print('### make branches in Git repositories')
+        print('###\n### make branches in Git repositories')
         for name in (
         'branch_name',
-        'branch_name_preamble',
-        'branch_name_preamble_repos',
         ):
-            print("### %-26s = %s" % (name, locals()[name]))
+            print("### %-35s = %s" % (name, locals()[name]))
     print(BASH_PREAMBLE)
 
-    if tag_name_preamble_repos:
-        tag_name_preamble_repos = re.compile(tag_name_preamble_repos)
-    if branch_name_preamble_repos:
-        branch_name_preamble_repos = re.compile(branch_name_preamble_repos)
+    repository_names_to_include_compiled = re.compile(repository_names_to_include_pattern) if repository_names_to_include_pattern else ''
+    repository_names_to_exclude_compiled = re.compile(repository_names_to_exclude_pattern) if repository_names_to_exclude_pattern else ''
 
+    repos_to_action = {}
     with open('artifacts_to_archive/head_commits.txt', 'r') as commits_file:
         for line in commits_file:  # standard OS terminator is converted to \n
             line = line.rstrip('\n') # remove trailing newline
             if line.startswith('#') or (not line.strip()):
                 continue
-            repo_to_action = {}
             # note that BRANCH is the checked-out branch in the original build job (usually master), _not_ the new branch we are making
             # a commit can of course be on more than one branch
+            repo_name = None
+            repo_description = {}
             for token in ('repository', 'URL', 'HEAD', 'BRANCH'):
                 try:
                     i = line.index(token + '=')
@@ -123,20 +122,45 @@ git branch -a'''
                         continue
                     print('*** ERROR looking for token "%s" in string "%s"' % (token, line))
                     raise
-                repo_to_action[token] = line[i+len(token)+1:j]
-            if repo_to_action['repository'] in REPOSITORIES_TO_IGNORE:
-                print('\n# skipping %(repository)s, since it is in the ignore list' % {'repository': repo_to_action['repository']})
-                continue
-            if repo_to_action['URL'].startswith(github_anonymous_prefix):
-                repo_to_action['URL'] = github_authenticated_prefix + repo_to_action['URL'][len(github_anonymous_prefix):]  # convert URL from anonymous clone to authenticated
-            print(BASH_GIT_PREAMBLE % {'repository': repo_to_action['repository'], 'URL': repo_to_action['URL']})
-            if tag_name and tag_commitmsg:
-                preamble = tag_name_preamble if tag_name_preamble and tag_name_preamble_repos and tag_name_preamble_repos.match(repo_to_action['repository']) else ''
-                print(BASH_GIT_TAG % {'tag_name_preamble': preamble, 'tag_name': tag_name, 'tag_commitmsg': tag_commitmsg, 'HEAD': repo_to_action['HEAD']})
-            if branch_name:
-                preamble = branch_name_preamble if branch_name_preamble and branch_name_preamble_repos and branch_name_preamble_repos.match(repo_to_action['repository']) else ''
-                print(BASH_GIT_BRANCH % {'branch_name_preamble': preamble, 'branch_name': branch_name, 'HEAD': repo_to_action['HEAD']})
-        print('\ncd ${work_dir}/..\n')
+                if token == 'repository':
+                    repo_name = line[i+len(token)+1:j]
+                else:
+                    repo_description[token] = line[i+len(token)+1:j]
+            repos_to_action[repo_name] = repo_description
+
+    skip_count = 0
+    for repo_name in sorted(repos_to_action):
+        if repo_name in REPOSITORIES_TO_ALWAYS_IGNORE:
+            print('# skipping %(repository)-35s since it is in the global ignore list' % {'repository': repo_name})
+            skip_count += 1
+            del repos_to_action[repo_name]
+            continue
+        if repository_names_to_include_compiled and not repository_names_to_include_compiled.match(repo_name):
+            print('# skipping %(repository)-35s since it does not match the include pattern: %(pattern)s' % {'repository': repo_name, 'pattern': repository_names_to_include_pattern})
+            del repos_to_action[repo_name]
+            skip_count += 1
+            continue
+        if repository_names_to_exclude_compiled and repository_names_to_exclude_compiled.match(repo_name):
+            print('# skipping %(repository)-35s since it matches the exclude pattern: %(pattern)s' % {'repository': repo_name, 'pattern': repository_names_to_exclude_pattern})
+            del repos_to_action[repo_name]
+            skip_count += 1
+            continue
+    if skip_count:
+        print('\n# skipped %s repositories' % (skip_count,))
+
+    action_count = 0
+    for repo_name in sorted(repos_to_action):
+        if repos_to_action[repo_name]['URL'].startswith(github_anonymous_prefix):
+            repos_to_action[repo_name]['URL'] = github_authenticated_prefix + repos_to_action[repo_name]['URL'][len(github_anonymous_prefix):]  # convert URL from anonymous clone to authenticated
+        print(BASH_GIT_PREAMBLE % {'repository': repo_name, 'URL': repos_to_action[repo_name]['URL']})
+        if tag_name and tag_commitmsg:
+            print(BASH_GIT_TAG % {'tag_name': tag_name, 'tag_commitmsg': tag_commitmsg, 'HEAD': repos_to_action[repo_name]['HEAD']})
+        if branch_name:
+            print(BASH_GIT_BRANCH % {'branch_name': branch_name, 'HEAD': repos_to_action[repo_name]['HEAD']})
+        action_count += 1
+
+    print('\n# generated action for %s repositories' % (action_count,))
+    print('\ncd ${work_dir}/..\n')
 
 ###############################################################################
 # Command line processing                                                     #
