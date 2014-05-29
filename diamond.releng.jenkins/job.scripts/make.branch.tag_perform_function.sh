2b06db3e7f10ac25d78b45339cf51a91257b6436
Matthew Webber <matthew.webber@diamond.ac.uk>
2014-05-29 16:15:20 +0100

    Improve text in case it is truncated upon display

--- diamond.releng.jenkins/job.scripts/make.branch.tag_perform_function.sh
+++ diamond.releng.jenkins/job.scripts/make.branch.tag_perform_function.sh
@@ -23,12 +23,12 @@ make_branch_tag_action_perform_function () {
     if [[ "$(echo ${make_branch_tag_action:-none} | tr '[:upper:]' '[:lower:]')" == *push* ]]; then
         echo "*** Script to perform tag or branch action generated, now running ..."
         . ${WORKSPACE}/artifacts_to_archive/make_branch_tag_script.sh
-        build_description_start="committed/pushed"
+        build_description_start="made"
     else
         echo "*** Script to perform tag or branch action generated, but will not be run"
         build_description_start="saved <a href=\"/job/${JOB_NAME}/${BUILD_NUMBER}/artifact/artifacts_to_archive/\">script</a> to"
     fi
-    echo "set-build-description: ${build_description_start} ${action_text} based on <a href=\"/job/${job_name_specifying_branch_point}/\">${job_name_specifying_branch_point}</a> build <a href=\"/job/${job_name_specifying_branch_point}/${copyartifact_build_number}/\">${copyartifact_build_number}</a>"
+    echo "set-build-description: ${build_description_start} ${action_text} as at build <a href=\"/job/${job_name_specifying_branch_point}/${copyartifact_build_number}/\">${copyartifact_build_number}</a> of <a href=\"/job/${job_name_specifying_branch_point}/\">${job_name_specifying_branch_point}</a>"
 
 }
 
