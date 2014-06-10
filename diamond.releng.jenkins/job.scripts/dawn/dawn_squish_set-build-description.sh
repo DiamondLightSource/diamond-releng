a379494c507de724cf7f59d6c5a913a39ed81566
Matthew Webber <matthew.webber@diamond.ac.uk>
2014-06-10 14:30:21 +0100

    Fix job description

--- diamond.releng.jenkins/job.scripts/dawn/dawn_squish_set-build-description.sh
+++ diamond.releng.jenkins/job.scripts/dawn/dawn_squish_set-build-description.sh
@@ -3,5 +3,5 @@
 . ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/copyartifact_get_build_number.sh
 
 if [ -n "${copyartifact_build_number}" ]; then
-    echo "set-build-description: testing <a href=\"/job/Dawn${Dawn_flavour}.${Dawn_release}-create.product/\">create-product</a> build <a href=\"/job/Dawn${Dawn_flavour}.${Dawn_release}-create.product/${copyartifact_build_number}/\">${copyartifact_build_number}</a> for ${platform_count} platforms"
+    echo "set-build-description: testing <a href=\"/job/Dawn${Dawn_flavour}.${Dawn_release}-create.product/\">create-product</a> build <a href=\"/job/Dawn${Dawn_flavour}.${Dawn_release}-create.product/${copyartifact_build_number}/\">${copyartifact_build_number}</a>"
 fi
