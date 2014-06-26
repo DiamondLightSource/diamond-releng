18c83db581a67de764a6bd173090110f8a97d1e5
Matthew Webber <matthew.webber@diamond.ac.uk>
2014-06-26 15:32:58 +0100

    ADMIN: support for new/upcoming releases

--- diamond.releng.buckminster/software/dawn.py
+++ diamond.releng.buckminster/software/dawn.py
@@ -48,6 +48,7 @@ COMPONENT_ABBREVIATIONS.append(('testfiles', 'gda', 'GDALargeTestFiles'))
 COMPONENT_CATEGORIES = (
     # category, version, CQuery, template_version, version_synonyms
     ('gda', 'master', 'gda-master.cquery', 'v2.6', ('master', 'trunk')),
+    ('gda', 'v8.42', 'gda-v8.42.cquery', 'v2.6', ('v8.42', '8.42', '842')),
     ('gda', 'v8.40', 'gda-v8.40.cquery', 'v2.6', ('v8.40', '8.40', '840')),
     ('gda', 'v8.39', 'gda-v8.39.cquery', 'v2.6', ('v8.39', '8.39', '839')),
     ('gda', 'v8.38', 'gda-v8.38.cquery', 'v2.6', ('v8.38', '8.38', '838')),
@@ -68,9 +69,11 @@ COMPONENT_CATEGORIES = (
     ('ida', 'v2.18', 'ida-v2.18.cquery', 'v2.3', ('v2.18', '2.18', '218')),
     ('ida', 'v2.17', 'ida-v2.17.cquery', 'v2.2', ('v2.17', '2.17', '217')),
     ('dawn', 'master', 'dawn-master.cquery', 'v2.6', ('master', 'trunk')),
+    ('dawn', '1.6', 'dawn-v1.6.cquery', 'v2.6', ('v1.6', '1.6')),
     ('dawn', '1.5', 'dawn-v1.5.cquery', 'v2.6', ('v1.5', '1.5')),
     ('dawn', '1.4.1', 'dawn-v1.4.1.cquery', 'v2.5', ('v1.4.1', '1.4.1')),
     ('dawn', '1.4', 'dawn-v1.4.cquery', 'v2.5', ('v1.4', '1.4')),
+    ('dawn', 'gda-8.42', 'dawn-gda-8.42.cquery', 'v2.6', ('gda-8.42', 'gda842')),
     ('dawn', 'gda-8.40', 'dawn-gda-8.40.cquery', 'v2.6', ('gda-8.40', 'gda840')),
     ('dawn', 'gda-8.38', 'dawn-gda-8.38.cquery', 'v2.6', ('gda-8.38', 'gda838')),
     ('dawn', 'gda-8.36', 'dawn-gda-8.36.cquery', 'v2.5', ('gda-8.36', 'gda836')),
