5b9c633412cc573c2df34b61678839f819413205
Matthew Webber <matthew.webber@diamond.ac.uk>
2014-09-15 10:33:52 +0100

    Script improvement

--- diamond.releng.jenkins/job.scripts/set-build-description_buildstep.groovy
+++ diamond.releng.jenkins/job.scripts/set-build-description_buildstep.groovy
@@ -1,16 +1,33 @@
-// This Groovy script must be run using a Jenkins "Execute system Groovy script" build step
-// A previous step must write a line in the log of the form defined in "pattern" below
+// This script sets the job "build description" (which appears in the Jenkins web interface), if one is available
+// To provide a build description, a previous build step must write a line in the log of the form defined in "pattern" below
 
+// Note that there are two very similar scripts where we set the build description:
+// (1) A script suitable for a build step in the middle of the job. This is run using a Jenkins "Execute system Groovy script" build step.
+//     This is typically used in jobs that use artifacts from an earlier job: the build description reports what the job is testing
+// (2) A script suitable for a post-build step at the end of the job. This is run using a Jenkins "Groovy Postbuild" post-build action.
+//     This is typically used in jobs that do a materialize: the build description reports if the materialize fails due to network errors
+
+// If you change one of the two scripts, you might need to make a matching change to the other also.
+
+// context when running as a system Groovy script
 def currentBuild = Thread.currentThread().executable
+
 def pattern = /^set-build-description: (.+)$/
 
-// Parsing 20 most recent lines of the build log
+// Look for a build description in the 20 most recent lines of the build log
 log = currentBuild.getLog(20)
+founddesc = false
 for (line in log) {
-    match = (line =~ pattern)
+   match = (line =~ pattern)
    if (match) {
-     currentBuild.setDescription(match[0][1])
-     out.println "[set-build-description.groovy] Build description was set to: " + match[0][1]
+     founddesc = true
      break
-  }
+   }
+}
+
+if (founddesc) {
+   currentBuild.setDescription(match[0][1])
+   out.println "[set-build-description_buildstep.groovy] Build description was set to: " + match[0][1]
+} else {
+   out.println "[set-build-description_buildstep.groovy] No build description found to set"
 }
