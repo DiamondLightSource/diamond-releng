d6a0dbe35a1004a8f0561d1ef7a157028a7c92b5
Matthew Webber <matthew.webber@diamond.ac.uk>
2015-01-23 16:46:00 +0000

    Collect and save materialize info early, even if build subsequently fails

--- diamond.releng.jenkins/job.scripts/dawn/dawn_create.product_main.sh
+++ diamond.releng.jenkins/job.scripts/dawn/dawn_create.product_main.sh
@@ -10,18 +10,19 @@
 
 materialize_function
 
-record_head_commits_function > ${WORKSPACE}/materialized_head_commits.txt
+materialized_info_path=${WORKSPACE}/artifacts_to_archive
+mkdir -pv ${materialized_info_path}/
+record_head_commits_function > ${materialized_info_path}/materialized_head_commits.txt
 # also record the current head in repos that might not have been materialized, but we still need to branch when making a release
 for extra_repo in "dawn-test.git"; do
-    if ! grep -q "${extra_repo}" ${WORKSPACE}/materialized_head_commits.txt; then
-        echo -n "repository=${extra_repo}***URL=git://github.com/DawnScience/${extra_repo}***" >> ${WORKSPACE}/materialized_head_commits.txt
-        echo -n "HEAD=$(git ls-remote git@github.com:DawnScience/${extra_repo} refs/heads/master | cut -f 1)***" >> ${WORKSPACE}/materialized_head_commits.txt
-        echo "BRANCH=master***" >> ${WORKSPACE}/materialized_head_commits.txt
-        echo "Appended to materialized_head_commits.txt: $(tail -n 1 ${WORKSPACE}/materialized_head_commits.txt)"
+    if ! grep -q "${extra_repo}" ${materialized_info_path}/materialized_head_commits.txt; then
+        echo -n "repository=${extra_repo}***URL=git://github.com/DawnScience/${extra_repo}***" >> ${materialized_info_path}/materialized_head_commits.txt
+        echo -n "HEAD=$(git ls-remote git@github.com:DawnScience/${extra_repo} refs/heads/master | cut -f 1)***" >> ${materialized_info_path}/materialized_head_commits.txt
+        echo "BRANCH=master***" >> ${materialized_info_path}/materialized_head_commits.txt
+        echo "Appended to materialized_head_commits.txt: $(tail -n 1 ${materialized_info_path}/materialized_head_commits.txt)"
     fi
 done
-mv -v ${WORKSPACE}/materialized_head_commits.txt ${buckminster_root_prefix}/products/
-record_targetplatform_contents_function > ${buckminster_root_prefix}/products/materialized_targetplatform_contents.txt
+record_targetplatform_contents_function > ${materialized_info_path}/materialized_targetplatform_contents.txt
 
 if [[ "$(type -t pre_build_function)" == "function" ]]; then
     pre_build_function
