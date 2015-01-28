ace5ad7bc661a9ee66e151f3bf0fe99a7ec28b49
Matthew Webber <matthew.webber@diamond.ac.uk>
2015-01-28 14:41:08 +0000

    Add option to test a Gerrit change using either merge or rebase, as applicable

--- diamond.releng.jenkins/job.scripts/gda/gda_gerrit_pre.post.materialize.functions.sh
+++ diamond.releng.jenkins/job.scripts/gda/gda_gerrit_pre.post.materialize.functions.sh
@@ -1,4 +1,4 @@
-# handle branch switching for Gerrit test jobs (which should run with a materialize type of "recreate"). 
+# handle branch switching for Gerrit test jobs (which should run with a materialize type of "recreate")
 
 . ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/pre.materialize_checkout.standard.branches_function.sh
 
@@ -7,18 +7,33 @@ pre_materialize_function_1 () {
 }
 
 pre_materialize_function_2 () {
-    echo -e "\n*** `date +"%a %d/%b/%Y %H:%M:%S"` getting Gerrit patch ${GERRIT_PROJECT} ${GERRIT_REFSPEC} ***\n"
+    echo -e "\n*** `date +"%a %d/%b/%Y %H:%M:%S"` getting Gerrit patch ${GERRIT_PROJECT} ${GERRIT_REFSPEC} (branch ${repo_branch_name_for_release}) ***\n"
     repo=${materialize_workspace_path}_git/$(basename ${GERRIT_PROJECT}).git
     if [[ ! -d "${repo}" ]]; then
         mkdir -pv ${materialize_workspace_path}_git
         cd ${materialize_workspace_path}_git
         git clone ${GERRIT_SCHEME}://${GERRIT_HOST}:${GERRIT_PORT}/${GERRIT_PROJECT} ${repo}
-        git -C ${repo} checkout ${repo_branch_name_for_release}
     fi
+
+    # do the merge or rebase in a new local branch, to avoid any risk of pushing something back to the remote
     cd ${repo}
+    git checkout ${repo_branch_name_for_release}
+    git branch -D local-${JOBNAME} || true
+    git branch -vv --no-track local-${JOBNAME} remotes/origin/${repo_branch_name_for_release}
+    git checkout local-${JOBNAME}
     git fetch origin ${GERRIT_REFSPEC}
-    git checkout -f FETCH_HEAD
-    git rebase --verbose ${repo_branch_name_for_release}
-    git clean -fdx
+
+    # Merge or rebase the change on the main branch, using whatever method is specified for Gerrit's "Submit Type:" for the repository
+    submit_type=$(wget -q -O - "http://${GERRIT_HOST}:8080/projects/$(echo ${GERRIT_PROJECT} | sed 's#/#%2F#g')/config" | grep '"submit_type"')
+    if [[ "${submit_type}" == *REBASE_IF_NECESSARY* ]]; then
+        # option - attempt to rebase the change with the main branch
+        git checkout -f FETCH_HEAD
+        git rebase --verbose local-${JOBNAME}
+    else
+        # option - attempt to merge the change with the main branch
+        git merge --verbose FETCH_HEAD
+    fi
+
+    git log master^..
 
 }
