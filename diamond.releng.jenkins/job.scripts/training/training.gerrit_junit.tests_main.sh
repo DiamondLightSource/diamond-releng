3d64adcd41e794fdc7986722b2d4d5bbdee553b2
Matthew Webber <matthew.webber@diamond.ac.uk>
2015-02-04 16:02:39 +0000

    Put JUnit result file in a standard place

--- diamond.releng.jenkins/job.scripts/training/training.gerrit_junit.tests_main.sh
+++ diamond.releng.jenkins/job.scripts/training/training.gerrit_junit.tests_main.sh
@@ -2,11 +2,17 @@
 . ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/set-environment.sh GDA.${GDA_release}-environment-variables.properties
 
 # source functions that we will use
+. ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/junit.tests.clean_function.sh
 . ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/pre.post.materialize_functions.sh
 . ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/materialize_function.sh
 
 # remove old JUnit test report files from a previous run, so that Jenkins post-build cannot find them
-rm -f ${WORKSPACE}/TEST-gda.training.DummyTest.xml
+m_t="$(echo ${materialize_type:-unknown} | tr '[:upper:]' '[:lower:]' | cut -d ' ' -f1)"
+if [[ "${m_t}" != "extra-materialize" ]]; then
+if [[ "${m_t}" != "fresh" ]]; then
+    junit_tests_clean_function
+fi
+fi
 
 materialize_function
 
@@ -18,5 +24,7 @@ if [[ "${number}" == "0" ]]; then
 else
     file_to_copy=TEST-passed.xml
 fi
-cp -v ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/training/${file_to_copy} ${WORKSPACE}/TEST-gda.training.DummyTest.xml
+
+mkdir -pv ${materialize_workspace_path}_git/training-gerrit-1.git/test-reports/
+cp -vf ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/training/${file_to_copy} ${materialize_workspace_path}_git/training-gerrit-1.git/test-reports/TEST-gda.training.DummyTest.xml
 
