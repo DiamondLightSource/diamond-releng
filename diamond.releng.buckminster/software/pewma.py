553b92483aeb0a39f43c4ef40d2d586250a7f23a
Matthew Webber <matthew.webber@diamond.ac.uk>
2015-02-06 10:43:29 +0000

    Update beamline list

--- diamond.releng.buckminster/software/pewma.py
+++ diamond.releng.buckminster/software/pewma.py
@@ -32,9 +32,9 @@ for gda_config in (
     'lpd',                                              # Rutherford
     'all-dls', 'all-mt', 'all-mx',                      # Diamond
     'b16', 'b18', 'b21', 'b23', 'b24', 'dls', 'excalibur',
-    'i02', 'i03', 'i04', 'i04-1', 'i05', 'i06', 'i07', 'i08', 'i09', 'i09-1', 'i09', 'i09-2',
+    'i02', 'i03', 'i04', 'i04-1', 'i05', 'i06', 'i07', 'i08', 'i09', 'i09-1', 'i09-2',
     'i10', 'i11', 'i11-1', 'i12', 'i13i', 'i13j', 'i15', 'i16', 'i18',
-    'i19', 'i20', 'i20-1', 'i22', 'i23', 'i24', 'lab11', 'mt', 'mx', 'ncdsim'):
+    'i19', 'i19.eh1', 'i19.eh2', 'i20', 'i20-1', 'i22', 'i23', 'i24', 'lab11', 'mt', 'mx', 'ncdsim'):
     COMPONENT_ABBREVIATIONS.append((gda_config, 'gda', gda_config+'-config'))  # recognise name without -config suffix
     COMPONENT_ABBREVIATIONS.append((gda_config+'-config', 'gda', gda_config+'-config'))  # recognise name with -config suffix
 
