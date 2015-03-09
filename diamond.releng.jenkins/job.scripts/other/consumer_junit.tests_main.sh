6f8202bc66ad3d1e4b08fdf08ac29b8044bdec2b
Matthew Webber <matthew.webber@diamond.ac.uk>
2015-03-09 15:29:26 +0000

    Add consumer tests for Matt Gerring

---
+++ diamond.releng.jenkins/job.scripts/other/consumer_junit.tests_main.sh
@@ -0,0 +1,34 @@
+# specify the environment
+. ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/set-environment.sh
+
+# source functions that we will use
+. ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/junit.tests.clean_function.sh
+. ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/materialize_function.sh
+. ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/record_head_commits_function.sh
+. ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/record_targetplatform_contents_function.sh
+. ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/build_function.sh
+. ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/junit_function.sh
+
+# remove old JUnit test report files from a previous run, so that Jenkins post-build cannot find them
+m_t="$(echo ${materialize_type:-unknown} | tr '[:upper:]' '[:lower:]' | cut -d ' ' -f1)"
+if [[ "${m_t}" != "extra-materialize" ]]; then
+if [[ "${m_t}" != "fresh" ]]; then
+    junit_tests_clean_function
+fi
+fi
+
+materialize_function
+
+mkdir -pv ${WORKSPACE}/artifacts_to_archive/
+record_head_commits_function > ${WORKSPACE}/artifacts_to_archive/materialized_head_commits.txt
+record_targetplatform_contents_function > ${WORKSPACE}/artifacts_to_archive/materialized_targetplatform_contents.txt
+
+if [[ "$(type -t pre_build_function)" == "function" ]]; then
+    pre_build_function
+fi
+build_function
+if [[ "$(type -t post_build_function)" == "function" ]]; then
+    post_build_function
+fi
+
+junit_function
