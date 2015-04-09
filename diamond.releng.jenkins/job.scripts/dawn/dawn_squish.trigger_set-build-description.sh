a239d1aa8a95fdd1af4aed325e282b8c9d1627df
Matthew Webber <matthew.webber@diamond.ac.uk>
2015-04-09 16:01:49 +0100

    Fix build description

--- diamond.releng.jenkins/job.scripts/dawn/dawn_squish.trigger_set-build-description.sh
+++ diamond.releng.jenkins/job.scripts/dawn/dawn_squish.trigger_set-build-description.sh
@@ -2,8 +2,8 @@
 . ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/set-environment.sh
 . ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/copyartifact_get_build_number.sh
 
-platform_count=$( (set -o posix ; set) | grep "trigger_squish" | grep "true" | wc -l)
+platform_count=$( (set -o posix ; set) | grep "trigger_squish" | grep -v "at_least_one_trigger_squish_parameter_selected" | grep "true" | wc -l)
 
 if [ -n "${copyartifact_build_number}" ]; then
-    echo "set-build-description: triggering <a href=\"/job/Dawn${Dawn_flavour}.${Dawn_release}-create.product/\">create-product</a> build <a href=\"/job/Dawn${Dawn_flavour}.${Dawn_release}-create.product/${copyartifact_build_number}/\">${copyartifact_build_number}</a> test (${platform_count} jobs)"
+    echo "set-build-description: triggering ${platform_count} test jobs of <a href=\"/job/${Dawn_upstream_product_job}/\">create-product${Dawn_squish_platform_job_suffix}</a> build <a href=\"/job/${Dawn_upstream_product_job}/${copyartifact_build_number}/\">${copyartifact_build_number}</a>"
 fi
