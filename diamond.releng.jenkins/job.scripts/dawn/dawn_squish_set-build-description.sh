d5f04e1b33ece101e217f0ccace65ac189162207
Matthew Webber <matthew.webber@diamond.ac.uk>
2015-04-17 16:04:50 +0100

    Handle embedded blanks in match string

--- diamond.releng.jenkins/job.scripts/dawn/dawn_squish_set-build-description.sh
+++ diamond.releng.jenkins/job.scripts/dawn/dawn_squish_set-build-description.sh
@@ -2,9 +2,9 @@
 . ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/set-environment.sh
 . ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/copyartifact_get_build_number.sh
 
-if [[ "${product_origin}" == *Upload a .zip file* ]]; then
+if [[ "${product_origin}" == *"Upload a .zip file"* ]]; then
     echo "set-build-description: testing uploaded product .zip"
-elif [[ "${product_origin}" == *already on the test machine* ]]; then
+elif [[ "${product_origin}" == *"already on the test machine"* ]]; then
     echo "set-build-description: testing product .zip already on the test machine"
 elif [ -n "${copyartifact_build_number}" ]; then
     echo "set-build-description: testing <a href=\"/job/${DAWN_upstream_product_job}/\">create-product</a> build <a href=\"/job/${DAWN_upstream_product_job}/${copyartifact_build_number}/\">${copyartifact_build_number}</a>"
