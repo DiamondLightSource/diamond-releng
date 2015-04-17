bece59dd7a74a442060356f6927cbea01b9a404a
Matthew Webber <matthew.webber@diamond.ac.uk>
2015-04-17 15:18:32 +0100

    Cleanup old uploaded artifacts

--- diamond.releng.jenkins/job.scripts/squish_setup.sh
+++ diamond.releng.jenkins/job.scripts/squish_setup.sh
@@ -12,6 +12,10 @@ rm -f ${WORKSPACE}/squish_results.zip
 # delete any previous GDA server logs
 rm -rf ${WORKSPACE}/server_test_logs
 
+# if no artifacts were uploaded for this job, delete any uploads left over from an earlier run
+if [[ "$(printenv | grep "Upload a .zip file" | wc -l)" == "0" ]]; then
+    rm -rf ${WORKSPACE}/artifacts_to_test_uploaded
+fi
 # get Squish license key, if one is defined and we can read it
 if [[ -f "${SQUISH_LICENSE_FILE}" ]]; then
     cp -pv ${SQUISH_LICENSE_FILE} ${SQUISH_CONTROL_DIR}/ || true
