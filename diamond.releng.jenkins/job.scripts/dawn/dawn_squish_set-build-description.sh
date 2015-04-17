5ca72f364ac28b78e60c10ff32a57a99584cfe33
Matthew Webber <matthew.webber@diamond.ac.uk>
2015-04-17 14:56:30 +0100

    Handle more options when setting build description

--- diamond.releng.jenkins/job.scripts/dawn/dawn_squish_set-build-description.sh
+++ diamond.releng.jenkins/job.scripts/dawn/dawn_squish_set-build-description.sh
@@ -2,6 +2,12 @@
 . ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/set-environment.sh
 . ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/copyartifact_get_build_number.sh
 
-if [ -n "${copyartifact_build_number}" ]; then
-    echo "set-build-description: testing <a href=\"/job/Dawn${Dawn_flavour}.${Dawn_release}-create.product/\">create-product</a> build <a href=\"/job/Dawn${Dawn_flavour}.${Dawn_release}-create.product/${copyartifact_build_number}/\">${copyartifact_build_number}</a>"
+if [[ "${product_origin}" == *Upload a .zip file* ]]; then
+    echo "set-build-description: testing uploaded product .zip"
+elif [[ "${product_origin}" == *already on the test machine* ]]; then
+    echo "set-build-description: testing product .zip already on the test machine"
+elif [ -n "${copyartifact_build_number}" ]; then
+    echo "set-build-description: testing <a href=\"/job/${DAWN_upstream_product_job}/\">create-product</a> build <a href=\"/job/${DAWN_upstream_product_job}/${copyartifact_build_number}/\">${copyartifact_build_number}</a>"
+else
+    echo "set-build-description: Internal error: could not determine what was tested"
 fi
