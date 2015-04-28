4db34c6ec4934371778976e1620740829227bf00
Matthew Webber <matthew.webber@diamond.ac.uk>
2015-04-28 13:41:42 +0100

    Fix pattern for trapping errors

--- diamond.releng.buckminster/software/pewma.py
+++ diamond.releng.buckminster/software/pewma.py
@@ -125,7 +125,7 @@ JGIT_ERROR_PATTERNS = ( # JGit error messages that identify an intermittent chec
     ('org\.eclipse\.jgit\.api\.errors\.TransportException: (Connection reset|Short read of block\.)', 'Network error'),  # text = no specifc repository identified
     ('org\.apache\.http\.conn\.HttpHostConnectException: Connection to .+ refused', 'Connection refused'),  # text = no specifc repository identified
     ('HttpComponents connection error response code (500|502|503)', 'Server error'),  # text = no specifc repository identified
-    ('ERROR +No repository found at http://www\.opengda\.org/', 'Server error'),  # text = no specifc repository identified
+    ('ERROR: No repository found at http://www\.opengda\.org/', 'Server error'),  # text = no specifc repository identified
     )
 
 GERRIT_REPOSITORIES = (
