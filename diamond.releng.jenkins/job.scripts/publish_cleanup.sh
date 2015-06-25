f40e6584ab8b9ef7628cc12e6e5468339467bf8c
Matthew Webber <matthew.webber@diamond.ac.uk>
2015-06-25 09:53:25 +0100

    Improve description

--- diamond.releng.jenkins/job.scripts/publish_cleanup.sh
+++ diamond.releng.jenkins/job.scripts/publish_cleanup.sh
@@ -1,3 +1,3 @@
-# delete any previously published artifacts
+# delete workspace copy of artifacts to publish - done at start and end of publish jobs
 rm -rf ${WORKSPACE}/artifacts_to_publish
 rm -rf ${WORKSPACE}/p2_site_unzipped
