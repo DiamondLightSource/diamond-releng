a05627fe3ea667c449e1b087179407569b351aca
Matthew Webber <matthew.webber@diamond.ac.uk>
2015-08-14 14:56:58 +0100

    Write out report file before exiting

--- diamond.releng.jenkins/job.scripts/pre.post.materialize_functions.sh
+++ diamond.releng.jenkins/job.scripts/pre.post.materialize_functions.sh
@@ -15,10 +15,19 @@ fi
 
 if [[ "${gerrit_manual_test}" == "true" ]]; then
   mkdir -pv ${WORKSPACE}/artifacts_to_archive
+
   script=gerrit_manual_get_changes.py
   echo "Running ${script}"
+  set +e  # Turn off errexit
   python ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/${script}
+  RETVAL=$?
+  set -e  # Turn on errexit
   cat ${WORKSPACE}/artifacts_to_archive/gerrit_changes_tested.txt
+  if [[ "${RETVAL}" != "0" ]]; then
+      echo "Problems with specification of changes to test, so exiting"
+      exit 1
+  fi
+
   script=artifacts_to_archive/gerrit.manual_pre.post.materialize.functions.sh
   echo "Sourcing ${script}"
   . ${WORKSPACE}/${script}
