2be088d49e52a2e65f6a44957cbf3b62cd826e2f
Matthew Webber <matthew.webber@diamond.ac.uk>
2015-10-01 11:47:28 +0100

    Fix find for tests to skip

--- diamond.releng.jenkins/job.scripts/junit.ignore.selected.tests_function.sh
+++ diamond.releng.jenkins/job.scripts/junit.ignore.selected.tests_function.sh
@@ -8,8 +8,8 @@ junit_ignore_selected_tests () {
 
     if [[ -n "${testnames_to_ignore_pattern}" ]]; then
         if [[ -d "${materialize_workspace_path}_git" ]]; then
-            find ${materialize_workspace_path}_git -type f -name "TEST-${testnames_to_ignore_pattern}\.xml" || true
-            find ${materialize_workspace_path}_git -type f -name "TEST-${testnames_to_ignore_pattern}\.xml" -exec mv -v {} {}.IGNORED \; || true
+            find ${materialize_workspace_path}_git -type f -regextype posix-extended -regex ".*/TEST-${testnames_to_ignore_pattern}\.xml" || true
+            find ${materialize_workspace_path}_git -type f -regextype posix-extended -regex ".*/TEST-${testnames_to_ignore_pattern}\.xml" -exec mv -v {} {}.IGNORED \; || true
         fi
     fi
 
