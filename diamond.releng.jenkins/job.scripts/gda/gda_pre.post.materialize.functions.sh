b5bc8a8d4952803a109f40d9907aee2e197ef8d5
Matthew Webber <matthew.webber@diamond.ac.uk>
2016-02-18 13:12:31 +0000

    Interim change to allow gdaserver to materialize and build

--- diamond.releng.jenkins/job.scripts/gda/gda_pre.post.materialize.functions.sh
+++ diamond.releng.jenkins/job.scripts/gda/gda_pre.post.materialize.functions.sh
@@ -3,7 +3,10 @@
 
 post_materialize_function_gda () {
 
-  :  # placeholder function not currently in use
+    if [[ "${non_beamline_product}" == "gdaserver" ]]; then
+        # need mapping-config (but none of its dependencies) in the workspace
+        ${pewma_py} ${log_level_option} -w ${materialize_workspace_path} ${materialize_location_option} ${materialize_properties_base} ${materialize_properties_extra} -Donly_..-config ${buckminster_osgi_areas} ${buckminster_extra_vmargs} materialize mapping-config ${materialize_category} ${materialize_version} || return 1
+    fi
 }
 
 #------------------------------------#
