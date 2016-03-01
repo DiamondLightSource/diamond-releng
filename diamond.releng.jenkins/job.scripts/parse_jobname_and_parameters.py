7e9aa7de37c1c989fa096f2a2077740b0252c22c
Matthew Webber <matthew.webber@diamond.ac.uk>
2016-03-01 10:51:06 +0000

    Further jobname parsing

--- diamond.releng.jenkins/job.scripts/parse_jobname_and_parameters.py
+++ diamond.releng.jenkins/job.scripts/parse_jobname_and_parameters.py
@@ -12,19 +12,29 @@ import cStringIO
 
 def parse_jenkins_jobname(jobname):
     parse_result = []  # use a list of tuples rather than a dictionary, so insertion order is maintained (OrderedDict not available pre-Python 2.7)
-    parse_result.append(('download.public', 'download.public' in jobname))
+
+    if 'download.public' in jobname:
+        parse_result.append(('download.public', True))
+        parse_result.append(('pewma_py_use_public_version', True))
+        parse_result.append(('buckminster_headless_use_public_version', True))
+        parse_result.append(('materialize_location_option', '--location=public'))
+    else:
+        parse_result.append(('download.public', False))
 
     #########################
     # Parsing for Dawn jobs #
     #########################
 
-    m = re.match('^Dawn(?P<DAWN_flavour>Diamond|Vanilla)\.(?P<DAWN_release>[^-]+)-.+?(download\.public)?(~(?P<job_variant>.+))?$', jobname)
+    m = re.match('^Dawn(?P<DAWN_flavour>Diamond|Vanilla)\.(?P<DAWN_release>[^-]+)-.+?(-download\.public)?(~(?P<job_variant>.+))?$', jobname)
     if m:
         for part in ('DAWN_flavour', 'DAWN_release', 'job_variant'):
             parse_result.append((part, m.groupdict()[part]))
 
         # if this is a create.product job, work out the name of the two downstream jobs (the publish.snapshot job, and the squish trigger job)
         if 'create.product' in jobname:
+            parse_result.append(('materialize_properties_extra', "'-Dskip_ALL_test_fragments=true'"))
+            parse_result.append(('build_options_extra', '--suppress-compile-warnings'))
+            parse_result.append(('product_options_extra', '--suppress-compile-warnings'))
             parse_result.append(('publish_snapshot_job_to_trigger', jobname.replace('-create.product', '-publish.snapshot')))
             parse_result.append(('squish_trigger_job_to_trigger', jobname.replace('-create.product', '-squish.trigger')))
 
@@ -50,7 +60,7 @@ def parse_jenkins_jobname(jobname):
     # Parsing for GDA jobs #
     #########################
 
-    m = re.match('^GDA\.(?P<GDA_release>[^-]+)-(.+?)(download\.public)?(~(?P<job_variant>.+))?$', jobname)
+    m = re.match('^GDA\.(?P<GDA_release>[^-]+)-(.+?)(-download\.public)?(~(?P<job_variant>.+))?$', jobname)
     if m:
         for part in ('GDA_release', 'job_variant'):
             parse_result.append((part, m.groupdict()[part]))
@@ -60,17 +70,53 @@ def parse_jenkins_jobname(jobname):
         if '-gerrit-trigger' in job_subname:
             parse_result.append(('gerrit_job_to_trigger', jobname.replace('-gerrit-trigger', '-junit.tests-gerrit')))
 
+        if 'junit' in jobname:
+            parse_result.append(('materialize_component', 'all-dls-config'))
+
         # GDA create.product.beamline jobs (client)
         # if this is a create.product job, work out the name of the two downstream jobs (the publish.snapshot job, and the squish trigger job)
-        m = re.match('^create.product.beamline-(?P<site>[^-]+)-(?P<beamline>.+)$', job_subname)
+        m = re.match('^create.product.beamline-(?P<GDA_site>[^-]+)-(?P<GDA_beamline>.+)$', job_subname)
         if m:
+            site = m.group('GDA_site')
+            beamline = m.group('GDA_beamline')
+            parse_result.append(('GDA_beamline', beamline))
+            if beamline == 'synoptic':
+                materialize_component = uk.ac.gda.client.synoptic.site
+            else:
+                materialize_component = beamline + '-config'
+            if site == 'DLS':
+                if beamline in ('excalibur', 'synoptic'):
+                    product_site = 'uk.ac.gda.client.' + beamline + '.site'
+                elif beamline == 'i20-1':
+                    product_site = 'uk.ac.gda.beamline.i20_1.site'
+                else:
+                    product_site = 'uk.ac.gda.beamline.' + beamline + '.site'
+            elif site == 'GDA':
+                if beamline == 'logpanel':
+                    product_site = 'uk.ac.gda.client.' + beamline + '.site'
+                else:
+                    product_site = 'uk.ac.gda.' + beamline + '.site'
+            elif site == 'APS':
+                product_site = 'gov.aps.gda.beamline.' + beamline + '.site'
+            elif site == 'ESRF':
+                product_site = 'fr.esrf.gda.beamline.' + beamline + '.site'
+            elif site == 'RAL':
+                product_site = 'uk.ac.rl.gda.' + beamline + '.site'
+            parse_result.append(('materialize_component', materialize_component))
+            parse_result.append(('materialize_properties_extra', "'-Dskip_ALL_test_fragments=true'"))
+            parse_result.append(('build_options_extra', '--suppress-compile-warnings'))
+            parse_result.append(('product_site', product_site))
+            parse_result.append(('product_options_extra', '--suppress-compile-warnings'))
+            # the example beamline .properties filenames are different from the standard names defined in GDA.<release>-environment-variables.properties
+            if (site == 'GDA') and (beamline == 'example'):
+                parse_result.append(('buckminster_properties_filename', 'buckminster.diamond.jenkins.properties'))
             parse_result.append(('publish_snapshot_job_to_trigger', jobname.replace('-create.product.beamline', '-publish.snapshot')))
             parse_result.append(('squish_trigger_job_to_trigger' ,jobname.replace('-create.product.beamline', '-squish.trigger')))
 
-        # GDA create.product (other than client)jobs
+        # GDA create.product (other than client) jobs
         # if it's a create.product job, but not a create.product.beamline job; or a publish job, get the product name
         else:
-            m = re.match('^create.product-(?P<product_name>.+)(download\.public)?(~(?P<job_variant>.+))?$', job_subname)
+            m = re.match('^create.product-(?P<product_name>.+)(-download\.public)?(~(?P<job_variant>.+))?$', job_subname)
             if m:
                 parse_result.append(('non_beamline_product', m.group('product_name')))
                 parse_result.append(('publish_snapshot_job_to_trigger', jobname.replace('-create.product', '-publish.snapshot')))
@@ -94,14 +140,17 @@ def parse_jenkins_jobname(jobname):
             parse_result.append(('upstream_create_product_job', m.group('prefix') + '-create.product' + m.group('suffix')))
 
     # For JUnit jobs, post-build should scan for compiler warnings, and for open tasks, only in master
-    if 'junit' in jobname and ('gerrit' not in jobname):
-        scan = False
-        for (k,v) in parse_result:
-            if k.endswith('_release') and (v == 'master'):
-                scan = True
-                break
-        parse_result.append(('postbuild_scan_for_compiler_warnings', scan))
-        parse_result.append(('postbuild_scan_for_open_tasks', scan))
+    if 'junit' in jobname:
+        if 'gerrit' in jobname:
+            parse_result.append(('build_options_extra', '--suppress-compile-warnings'))
+        if 'gerrit' not in jobname:
+            scan = False
+            for (k,v) in parse_result:
+                if k.endswith('_release') and (v == 'master'):
+                    scan = True
+                    break
+            parse_result.append(('postbuild_scan_for_compiler_warnings', scan))
+            parse_result.append(('postbuild_scan_for_open_tasks', scan))
 
     # determine whether any publish_* parameter was set
     # determine whether any trigger_squish_* parameter was set
@@ -109,9 +158,9 @@ def parse_jenkins_jobname(jobname):
         at_least_one_publish_parameter_selected = False
         at_least_one_trigger_squish_parameter_selected = False
         for (k,v) in os.environ.iteritems():
-            if k.startswith('publish_'):
+            if k.startswith('publish_') and (v.lower == 'true'):
                 at_least_one_publish_parameter_selected = True
-            if k.startswith('trigger_squish_'):
+            if k.startswith('trigger_squish_') and (v.lower == 'true'):
                 at_least_one_trigger_squish_parameter_selected = True
             if at_least_one_publish_parameter_selected and at_least_one_trigger_squish_parameter_selected:
                 break
@@ -150,6 +199,9 @@ def test_parse_jenkins_jobname():
         ('DAWN_flavour', 'Diamond'),
         ('DAWN_release', '1.11'),
         ('job_variant', None),
+        ('materialize_properties_extra', "'-Dskip_ALL_test_fragments=true'"),
+        ('build_options_extra', '--suppress-compile-warnings'),
+        ('product_options_extra', '--suppress-compile-warnings'),
         ('publish_snapshot_job_to_trigger', 'DawnDiamond.1.11-publish.snapshot'),
         ('squish_trigger_job_to_trigger', 'DawnDiamond.1.11-squish.trigger'),
         ('at_least_one_publish_parameter_selected', False),
@@ -185,6 +237,9 @@ def test_parse_jenkins_jobname():
         ('DAWN_flavour', 'Diamond'),
         ('DAWN_release', '1.master'),
         ('job_variant', None),
+        ('materialize_properties_extra', "'-Dskip_ALL_test_fragments=true'"),
+        ('build_options_extra', '--suppress-compile-warnings'),
+        ('product_options_extra', '--suppress-compile-warnings'),
         ('publish_snapshot_job_to_trigger', 'DawnDiamond.1.master-publish.snapshot'),
         ('squish_trigger_job_to_trigger', 'DawnDiamond.1.master-squish.trigger'),
         ('at_least_one_publish_parameter_selected', False),
@@ -220,6 +275,9 @@ def test_parse_jenkins_jobname():
         ('DAWN_flavour', 'Diamond'),
         ('DAWN_release', 'master'),
         ('job_variant', None),
+        ('materialize_properties_extra', "'-Dskip_ALL_test_fragments=true'"),
+        ('build_options_extra', '--suppress-compile-warnings'),
+        ('product_options_extra', '--suppress-compile-warnings'),
         ('publish_snapshot_job_to_trigger', 'DawnDiamond.master-publish.snapshot'),
         ('squish_trigger_job_to_trigger', 'DawnDiamond.master-squish.trigger'),
         ('at_least_one_publish_parameter_selected', False),
@@ -241,9 +299,15 @@ def test_parse_jenkins_jobname():
     p = parse_jenkins_jobname('DawnVanilla.master-create.product-download.public')
     assert p == [
         ('download.public', True),
+        ('pewma_py_use_public_version', True),
+        ('buckminster_headless_use_public_version', True),
+        ('materialize_location_option', '--location=public'),
         ('DAWN_flavour', 'Vanilla'),
         ('DAWN_release', 'master'),
         ('job_variant', None),
+        ('materialize_properties_extra', "'-Dskip_ALL_test_fragments=true'"),
+        ('build_options_extra', '--suppress-compile-warnings'),
+        ('product_options_extra', '--suppress-compile-warnings'),
         ('publish_snapshot_job_to_trigger', 'DawnVanilla.master-publish.snapshot-download.public'),
         ('squish_trigger_job_to_trigger', 'DawnVanilla.master-squish.trigger-download.public'),
         ('at_least_one_publish_parameter_selected', False),
@@ -254,6 +318,9 @@ def test_parse_jenkins_jobname():
     p = parse_jenkins_jobname('DawnVanilla.master-publish.snapshot-download.public')
     assert p == [
         ('download.public', True),
+        ('pewma_py_use_public_version', True),
+        ('buckminster_headless_use_public_version', True),
+        ('materialize_location_option', '--location=public'),
         ('DAWN_flavour', 'Vanilla'),
         ('DAWN_release', 'master'),
         ('job_variant', None),
@@ -279,6 +346,9 @@ def test_parse_jenkins_jobname():
         ('DAWN_flavour', 'Diamond'),
         ('DAWN_release', 'master'),
         ('job_variant', 'neweclipse'),
+        ('materialize_properties_extra', "'-Dskip_ALL_test_fragments=true'"),
+        ('build_options_extra', '--suppress-compile-warnings'),
+        ('product_options_extra', '--suppress-compile-warnings'),
         ('publish_snapshot_job_to_trigger', 'DawnDiamond.master-publish.snapshot~neweclipse'),
         ('squish_trigger_job_to_trigger', 'DawnDiamond.master-squish.trigger~neweclipse'),
         ('at_least_one_publish_parameter_selected', False),
@@ -302,6 +372,7 @@ def test_parse_jenkins_jobname():
         ('download.public', False),
         ('GDA_release', 'master'),
         ('job_variant', None),
+        ('materialize_component', 'all-dls-config'),
         ('postbuild_scan_for_compiler_warnings', True),
         ('postbuild_scan_for_open_tasks', True),
         ]
@@ -312,6 +383,8 @@ def test_parse_jenkins_jobname():
         ('download.public', False),
         ('GDA_release', 'master'),
         ('job_variant', None),
+        ('materialize_component', 'all-dls-config'),
+        ('build_options_extra', '--suppress-compile-warnings'),
         ]
     write_parse_result(p, output)
 
@@ -320,6 +393,13 @@ def test_parse_jenkins_jobname():
         ('download.public', False),
         ('GDA_release', 'master'),
         ('job_variant', None),
+        ('GDA_beamline', 'example'),
+        ('materialize_component', 'example-config'),
+        ('materialize_properties_extra', "'-Dskip_ALL_test_fragments=true'"),
+        ('build_options_extra', '--suppress-compile-warnings'),
+        ('product_site', 'uk.ac.gda.example.site'),
+        ('product_options_extra', '--suppress-compile-warnings'),
+        ('buckminster_properties_filename', 'buckminster.diamond.jenkins.properties'),
         ('publish_snapshot_job_to_trigger', 'GDA.master-publish.snapshot-GDA-example'),
         ('squish_trigger_job_to_trigger', 'GDA.master-squish.trigger-GDA-example'),
         ('at_least_one_publish_parameter_selected', False),
@@ -330,8 +410,18 @@ def test_parse_jenkins_jobname():
     p = parse_jenkins_jobname('GDA.master-create.product.beamline-GDA-example-download.public')
     assert p == [
         ('download.public', True),
+        ('pewma_py_use_public_version', True),
+        ('buckminster_headless_use_public_version', True),
+        ('materialize_location_option', '--location=public'),
         ('GDA_release', 'master'),
         ('job_variant', None),
+        ('GDA_beamline', 'example'),
+        ('materialize_component', 'example-config'),
+        ('materialize_properties_extra', "'-Dskip_ALL_test_fragments=true'"),
+        ('build_options_extra', '--suppress-compile-warnings'),
+        ('product_site', 'uk.ac.gda.example.site'),
+        ('product_options_extra', '--suppress-compile-warnings'),
+        ('buckminster_properties_filename', 'buckminster.diamond.jenkins.properties'),
         ('publish_snapshot_job_to_trigger', 'GDA.master-publish.snapshot-GDA-example-download.public'),
         ('squish_trigger_job_to_trigger', 'GDA.master-squish.trigger-GDA-example-download.public'),
         ('at_least_one_publish_parameter_selected', False),
@@ -344,6 +434,12 @@ def test_parse_jenkins_jobname():
         ('download.public', False),
         ('GDA_release', 'master'),
         ('job_variant', None),
+        ('GDA_beamline', 'b16'),
+        ('materialize_component', 'b16-config'),
+        ('materialize_properties_extra', "'-Dskip_ALL_test_fragments=true'"),
+        ('build_options_extra', '--suppress-compile-warnings'),
+        ('product_site', 'uk.ac.gda.beamline.b16.site'),
+        ('product_options_extra', '--suppress-compile-warnings'),
         ('publish_snapshot_job_to_trigger', 'GDA.master-publish.snapshot-DLS-b16'),
         ('squish_trigger_job_to_trigger', 'GDA.master-squish.trigger-DLS-b16'),
         ('at_least_one_publish_parameter_selected', False),
@@ -369,8 +465,7 @@ def test_parse_jenkins_jobname():
     print('*** Completed: test_parse_jenkins_jobname()')
 
 if __name__ == '__main__':
-    # test_parse_jenkins_jobname()
+    test_parse_jenkins_jobname()
     p = parse_jenkins_jobname(os.environ['JOB_NAME'])
     write_parse_result(p)
-
 
