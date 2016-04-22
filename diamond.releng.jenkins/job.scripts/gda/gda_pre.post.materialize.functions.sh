aeb23189ba285825e7bc83cfe3449038117e6d81
Matthew Webber <matthew.webber@diamond.ac.uk>
2016-04-22 10:54:33 +0100

    mapping-config has been renamed to p45-config

--- diamond.releng.jenkins/job.scripts/gda/gda_pre.post.materialize.functions.sh
+++ diamond.releng.jenkins/job.scripts/gda/gda_pre.post.materialize.functions.sh
@@ -4,8 +4,8 @@
 post_materialize_function_gda () {
 
     if [[ "${non_beamline_product}" == "gdaserver" ]]; then
-        # need mapping-config (but none of its dependencies) in the workspace
-        ${pewma_py} ${log_level_option} -w ${materialize_workspace_path} ${materialize_location_option} ${materialize_properties_base} ${materialize_properties_extra} -Donly_..-config.common=true ${buckminster_osgi_areas} ${buckminster_extra_vmargs} materialize mapping-config ${materialize_category} ${materialize_version} || return 1
+        # need p45-config (but none of its dependencies) in the workspace
+        ${pewma_py} ${log_level_option} -w ${materialize_workspace_path} ${materialize_location_option} ${materialize_properties_base} ${materialize_properties_extra} -Donly_..-config.common=true ${buckminster_osgi_areas} ${buckminster_extra_vmargs} materialize p45-config ${materialize_category} ${materialize_version} || return 1
     fi
 }
 
