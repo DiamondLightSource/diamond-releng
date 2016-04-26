ba4b66f434b66428f1db48a831eebe28fa295391
Matthew Webber <matthew.webber@diamond.ac.uk>
2016-04-26 14:55:16 +0100

    Don't attempt to move files that are no longer in this location

--- diamond.releng.jenkins/job.scripts/gda/gda_archive-products.sh
+++ diamond.releng.jenkins/job.scripts/gda/gda_archive-products.sh
@@ -1,3 +1,2 @@
-mv -v ${WORKSPACE}/create.product.root/products/*.txt ${WORKSPACE}/artifacts_to_archive/
 mv -v ${WORKSPACE}/create.product.root/products/*linux64.zip ${WORKSPACE}/artifacts_to_archive/
 
