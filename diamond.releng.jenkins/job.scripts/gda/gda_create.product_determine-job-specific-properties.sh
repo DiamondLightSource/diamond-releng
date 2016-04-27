ab8849d8e14fdd215cd6362c0774d0bf195db39a
Matthew Webber <matthew.webber@diamond.ac.uk>
2016-04-27 15:25:51 +0100

    Revert part of earlier removal of files allegedly replaced by parse_jobname_and_parameters.py
    These are used by Jenkins build jobs prior to 9.0

---
+++ diamond.releng.jenkins/job.scripts/gda/gda_create.product_determine-job-specific-properties.sh
@@ -0,0 +1,74 @@
+# Write job-specific information into a file in the form of name=value pairs
+set +x  # Turn off xtrace
+
+properties_filename=${WORKSPACE}/job-specific-environment-variables.properties
+
+if [[ -z "${non_beamline_product}" ]]; then
+    if [[ "${GDA_beamline}" == "synoptic" ]]; then
+        materialize_component=uk.ac.gda.client.${GDA_beamline}.site
+    else
+        materialize_component=${GDA_beamline:-internal_error}-config
+    fi
+
+    if [[ "${beamline_site}" == "DLS" ]]; then
+        if [[ "${GDA_beamline}" == "excalibur" || "${GDA_beamline}" == "synoptic" ]]; then
+            product_site=uk.ac.gda.client.${GDA_beamline}.site
+        elif [[ "${GDA_beamline}" == "i20-1" ]]; then
+            product_site=uk.ac.gda.beamline.i20_1.site
+        else
+            product_site=uk.ac.gda.beamline.${GDA_beamline}.site
+        fi
+    elif [[ "${beamline_site}" == "GDA" ]]; then
+        if [[ "${GDA_beamline}" == "logpanel" ]]; then
+            product_site=uk.ac.gda.client.${GDA_beamline}.site
+        else
+            product_site=uk.ac.gda.${GDA_beamline}.site
+        fi
+    elif [[ "${beamline_site}" == "APS" ]]; then
+        product_site=gov.aps.gda.beamline.${GDA_beamline}.site
+    elif [[ "${beamline_site}" == "ESRF" ]]; then
+        product_site=fr.esrf.gda.beamline.${GDA_beamline}.site
+    elif [[ "${beamline_site}" == "RAL" ]]; then
+        product_site=uk.ac.rl.gda.${GDA_beamline}.site
+    else
+        echo "internal error determining product_site"
+        exit 2
+    fi
+elif [[ "${non_beamline_product}" == "gdaserver" ]]; then
+    materialize_component=uk.ac.diamond.daq.server.site
+    product_site=${materialize_component}
+elif [[ "${non_beamline_product}" == "logpanel" ]]; then
+    materialize_component=uk.ac.gda.client.logpanel.site
+    product_site=${materialize_component}
+else
+    echo "internal error determining materialize_component and product_site"
+    exit 2
+fi
+
+if [[ "${download_public:false}" == "true" ]]; then
+    cat << EOF >> ${properties_filename}
+pewma_py_use_public_version=true
+buckminster_headless_use_public_version=true
+materialize_location_option=--location=public
+materialize_properties_base=\${materialize_properties_base__public_download}
+EOF
+fi
+
+cat << EOF >> ${properties_filename}
+# Written `date +"%a %d/%b/%Y %H:%M:%S %z"` (${BUILD_URL:-\$BUILD_URL:missing})
+materialize_component=${materialize_component:-internal_error in gda_create.product_determine-job-specific-properties.sh}
+materialize_properties_extra='-Dskip_ALL_test_fragments=true'
+build_options_extra=--suppress-compile-warnings
+product_site=${product_site:-internal_error in gda_create.product_determine-job-specific-properties.sh}
+product_options_extra=--suppress-compile-warnings
+EOF
+
+# the example beamline .properties filenames are different from the standard names defined in GDA.<release>-environment-variables.properties
+if [[ "${beamline_site}" == "GDA" && "${GDA_beamline}" == "example" ]]; then
+    cat << EOF >> ${properties_filename}
+buckminster_properties_filename=buckminster.diamond.jenkins.properties
+EOF
+fi
+
+echo "[gda_create.product_determine-job-specific-properties.sh] wrote ${properties_filename} --->"
+cat ${properties_filename}
---
+++ diamond.releng.jenkins/job.scripts/gda/gda_junit.tests_determine-job-specific-properties.sh
@@ -0,0 +1,18 @@
+# Write job-specific information into a file in the form of name=value pairs
+set +x  # Turn off xtrace
+
+properties_filename=${WORKSPACE}/job-specific-environment-variables.properties
+
+cat << EOF >> ${properties_filename}
+# Written `date +"%a %d/%b/%Y %H:%M:%S %z"` (${BUILD_URL:-\$BUILD_URL:missing})
+materialize_component=all-dls-config
+EOF
+
+if [[ "${JOB_NAME:-noname}" == *gerrit* ]]; then
+    cat << EOF >> ${properties_filename}
+build_options_extra=--suppress-compile-warnings
+EOF
+fi
+
+echo "[gda_junit.tests_determine-job-specific-properties.sh] wrote ${properties_filename} --->"
+cat ${properties_filename}
