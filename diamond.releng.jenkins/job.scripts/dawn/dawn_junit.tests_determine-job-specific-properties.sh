8bd904630129be8c5329f4c9db7912ebd8dc170f
Matthew Webber <matthew.webber@diamond.ac.uk>
2016-04-28 09:03:16 +0100

    Further revert part of earlier removal of files allegedly replaced by parse_jobname_and_parameters.py

---
+++ diamond.releng.jenkins/job.scripts/dawn/dawn_junit.tests_determine-job-specific-properties.sh
@@ -0,0 +1,17 @@
+# Write job-specific information into a file in the form of name=value pairs
+set +x  # Turn off xtrace
+
+properties_filename=${WORKSPACE}/job-specific-environment-variables.properties
+
+cat << EOF >> ${properties_filename}
+# Written `date +"%a %d/%b/%Y %H:%M:%S %z"` (${BUILD_URL:-\$BUILD_URL:missing})
+EOF
+
+if [[ "${JOB_NAME:-noname}" == *gerrit* ]]; then
+    cat << EOF >> ${properties_filename}
+build_options_extra=--suppress-compile-warnings
+EOF
+fi
+
+echo "[dawn_junit.tests_determine-job-specific-properties.sh] wrote ${properties_filename} --->"
+cat ${properties_filename}
