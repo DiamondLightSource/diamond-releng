7d88746b7a1bd698dd0e51662797b3b8cf787449
Matthew Webber <matthew.webber@diamond.ac.uk>
2016-05-27 09:39:43 +0100

    When materializing gdaserver, also need mt-config

--- diamond.releng.jenkins/job.scripts/gda/gda_pre.post.materialize.functions.sh
+++ diamond.releng.jenkins/job.scripts/gda/gda_pre.post.materialize.functions.sh
@@ -4,8 +4,9 @@
 post_materialize_function_gda () {
 
     if [[ "${non_beamline_product}" == "gdaserver" ]]; then
-        # need p45-config (but none of its dependencies) in the workspace
+        # need p45-config and mt-config (but none of their dependencies) in the workspace
         ${pewma_py} ${log_level_option} -w ${materialize_workspace_path} ${materialize_location_option} ${materialize_properties_base} ${materialize_properties_extra} ${materialize_properties_special} -Donly_..-config.common=true ${buckminster_osgi_areas} ${buckminster_extra_vmargs} materialize p45-config ${materialize_category} ${materialize_version} || return 1
+        ${pewma_py} ${log_level_option} -w ${materialize_workspace_path} ${materialize_location_option} ${materialize_properties_base} ${materialize_properties_extra} ${materialize_properties_special} -Donly_..-config.common=true ${buckminster_osgi_areas} ${buckminster_extra_vmargs} materialize mt-config ${materialize_category} ${materialize_version} || return 1
     fi
 }
 
