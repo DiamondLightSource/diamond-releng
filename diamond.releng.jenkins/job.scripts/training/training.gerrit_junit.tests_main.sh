4d86d3913942050b0227af30541ae4aaf3bf8213
Matthew Webber <matthew.webber@diamond.ac.uk>
2018-02-05 16:55:11 +0000

    Update training jobs to latest way of doing things

--- diamond.releng.jenkins/job.scripts/training/training.gerrit_junit.tests_main.sh
+++ diamond.releng.jenkins/job.scripts/training/training.gerrit_junit.tests_main.sh
@@ -4,7 +4,12 @@
 # source functions that we will use
 . ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/junit.tests.clean_function.sh
 . ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/pre.post.materialize_functions.sh
+. ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/update_single_git_repo_function.sh
 . ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/materialize_function.sh
+. ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/record_materialization_functions.sh
+. ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/build_function.sh
+. ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/junit_function.sh
+. ${WORKSPACE}/diamond-releng.git/diamond.releng.jenkins/job.scripts/junit.ignore.selected.tests_function.sh
 
 # remove old JUnit test report files from a previous run, so that Jenkins post-build cannot find them
 m_t="$(echo ${materialize_type:-unknown} | tr '[:upper:]' '[:lower:]' | cut -d ' ' -f1)"
@@ -14,7 +19,13 @@ if [[ "${m_t}" != "fresh" ]]; then
 fi
 fi
 
+materialized_info_path=${WORKSPACE}/artifacts_to_archive
+
 materialize_function
+
+record_head_commits_function > ${materialized_info_path}/materialized_head_commits.txt
+record_workspace_projects_function > ${materialized_info_path}/materialized_project_names.txt
+record_targetplatform_contents_function > ${materialized_info_path}/materialized_targetplatform_contents.txt
 
 # simulate running JUnit tests - copy either a "passed" (chance: 3/4) or "failed" (chance: 1/4) result file
 number=$RANDOM
